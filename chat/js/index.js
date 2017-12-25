6f221c5013a9134bc37f3e7fbd6d12c9df75e358
xiezefeng0927 <511279537@qq.com>
2017-12-25 23:59:20 +0800

    调整header

--- chat/js/index.js
+++ chat/js/index.js
@@ -12,6 +12,26 @@
 				back: {
 					type: Boolean,
 					default: false
+				},
+				search: {
+					type: Boolean,
+					default: false
+				},
+				plus: {
+					type: Boolean,
+					default: false
+				},
+				ellipsis: {
+					type: Boolean,
+					default: false
+				},
+				user: {
+					type: Boolean,
+					default: false
+				},
+				camera: {
+					type: Boolean,
+					default: false
 				}
 			},
 			data: function() {
@@ -80,7 +100,8 @@
 			},
 			data: function() {
 				return {
-					back: true
+					back: true,
+					user: true
 				}
 			},
 			created: function() {
@@ -158,6 +179,7 @@
 			data: function() {
 				return {
 					msg: '个人资料',
+					ellipsis: true,
 					back: true,
 					talking: false,
 					fromDetail: true   // 进入 talk 页面过渡动画的标识
@@ -190,6 +212,9 @@
 			},
 			data:function() {
 				return {
+					msg: '对 话',
+					search: true,
+					plus: true,
 					friend: {},
 					talking: false
 				}
@@ -260,6 +285,9 @@
 			},
 			data: function() {
 				return {
+					msg: '通信录',
+					search: true,
+					plus: true,
 					person: {},
 					detail: false
 				}
@@ -305,6 +333,132 @@
 		};
 		/* END */
 		
+		/* 课程导航栏 */
+		var SUBJECTNAV = {
+			template: '#subject-nav',
+			props: {
+				content: {
+					type: Object
+				}
+			},
+			data: function() {
+				return {
+					activeIndex: 0
+				}
+			},
+			created: function() {
+				this.$nextTick(function() {
+					this._initScroll();
+				});
+			},
+			methods: {
+				navClick: function(event, index) {
+					if(!event._constructed) {   // 阻止 click 的默认事件
+						return false;
+					}
+					this.$emit("transmit-index", index);
+				},
+				_initScroll: function() {
+					if(!this.navScroll) {
+						this.navScroll = new IScroll(this.$refs.nav, {
+							click: true
+						});
+					} else {
+						this.navScroll.refresh();
+					}
+				},
+				outIndex: function(currentIndex) {   // 给父组件调用，并接收参数
+					this.activeIndex = currentIndex;
+				}
+			},
+			watch: {
+				'content': function() {
+					this.$nextTick(function() {
+						this._initScroll();
+					});
+				}
+			}
+		}
+		/* END */
+		
+		/* 课程内容 */
+		var SUBJECTCONTENT = {
+			template: '#subject-content',
+			props: {
+				subContent: {
+					type: Object
+				}
+			},
+			data: function() {
+				return {
+					listHeight: [],
+					scrollY: 0
+				}
+			},
+			created: function() {
+				this.$nextTick(function() {
+					this._initScroll();
+				});
+			},
+			computed: {
+				currentIndex: function () {
+					for(var i = 0, len = this.listHeight.length; i < len; i++){
+						// 获取两个区间值
+						var height_1 = this.listHeight[i];
+						var height_2 = this.listHeight[i + 1];
+						if(!height_2 || (this.scrollY >= height_1 && this.scrollY < height_2)) {
+							return i;
+						}
+					}
+					return 0;
+				}
+			},
+			methods: {
+				_initScroll: function() {
+					var self = this;
+					if(!this.contentScroll) {
+						this.contentScroll = new IScroll(this.$refs.content, {
+							probeType: 3,    //表示能实时监听到 IScroll 滚动的位置
+							click: true
+						});
+					} else {
+						this.contentScroll.refresh();
+					}
+					this.contentScroll.on("scroll", function() {
+						self.scrollY = Math.abs( Math.ceil( this.y ) ) + 1;   //优化取值，由于 y 可能是一个带小数的值会损失精度，为满足体验效果所以 + 1.
+						self.$emit('transmit-current', self.currentIndex); // 向外派发出内容栏当前滚动到的元素是第几个
+					})
+				},
+				_calculateHeight: function() {  // 计算区间值
+					var listHook = this.$refs.list.getElementsByClassName("single-hook");
+					var height = 0;
+					this.listHeight.push(height);
+					for(var i = 0, len = listHook.length; i < len; i++) {
+						height += listHook[i].clientHeight;
+						this.listHeight.push(height);
+					}
+				},
+				outIndex: function (index) {  //给父组件调用，并接收参数
+					var listHook = this.$refs.list.getElementsByClassName("single-hook");
+					this.contentScroll.scrollToElement(listHook[index], 300);
+				}
+			},
+			filters: {
+				farmatTime: function(date) {
+					return new Date(date).pattern("yyyy-MM-dd");
+				}
+			},
+			watch: {
+				'subContent': function() {
+					this.$nextTick(function() {
+						this.contentScroll.refresh();
+						this._calculateHeight();
+					});
+				}
+			}
+		}
+		/* END */
+		
 		/* ps-grade-one */
 		var psGradeOne = {
 			template: "#ps-grade-one",
@@ -315,83 +469,353 @@
 			},
 			created: function() {
 				this.$http.get('data/studio.json').then(function(response) {
-					console.log(response.body.grade_one);
 					this.grade_one = response.body.grade_one; 
 				}, function(error) {
 					console.log(error);
 				});
 			},
-			filters: {
-				farmatTime: function(date) {
-					return new Date(date).pattern("yyyy-MM-dd");
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
 				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
 			}
 		}
 		/* END */
 		
 		/* ps-grade-two */
 		var psGradeTwo = {
-			template: "#ps-grade-two"
+			template: "#ps-grade-two",
+			data: function() {
+				return {
+					grade_two: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.grade_two = response.body.grade_two; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ps-grade-three */
 		var psGradeThree = {
-			template: "#ps-grade-three"
+			template: "#ps-grade-three",
+			data: function() {
+				return {
+					grade_three: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.grade_three = response.body.grade_three; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ps-grade-four */
 		var psGradeFour = {
-			template: "#ps-grade-four"
+			template: "#ps-grade-four",
+			data: function() {
+				return {
+					grade_four: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.grade_four = response.body.grade_four; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ps-grade-five */
 		var psGradeFive = {
-			template: "#ps-grade-five"
+			template: "#ps-grade-five",
+			data: function() {
+				return {
+					grade_five: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.grade_five = response.body.grade_five; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ps-grade-six */
 		var psGradeSix = {
-			template: "#ps-grade-six"
+			template: "#ps-grade-six",
+			data: function() {
+				return {
+					grade_six: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.grade_six = response.body.grade_six; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ms-grade-one */
 		var msGradeOne = {
-			template: "#ms-grade-one"
+			template: "#ms-grade-one",
+			data: function() {
+				return {
+					m_grade_one: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.m_grade_one = response.body.m_grade_one; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ms-grade-two */
 		var msGradeTwo = {
-			template: "#ms-grade-two"
+			template: "#ms-grade-two",
+			data: function() {
+				return {
+					m_grade_two: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.m_grade_two = response.body.m_grade_two; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* ms-grade-three */
 		var msGradeThree = {
-			template: "#ms-grade-three"
+			template: "#ms-grade-three",
+			data: function() {
+				return {
+					m_grade_three: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.m_grade_three = response.body.m_grade_three; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* hs-grade-one */
 		var hsGradeOne = {
-			template: "#hs-grade-one"
+			template: "#hs-grade-one",
+			data: function() {
+				return {
+					h_grade_one: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.h_grade_one = response.body.h_grade_one; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* hs-grade-two */
 		var hsGradeTwo = {
-			template: "#hs-grade-two"
+			template: "#hs-grade-two",
+			data: function() {
+				return {
+					h_grade_two: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.h_grade_two = response.body.h_grade_two; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
 		/* hs-grade-three */
 		var hsGradeThree = {
-			template: "#hs-grade-three"
+			template: "#hs-grade-three",
+			data: function() {
+				return {
+					h_grade_three: {}
+				}
+			},
+			created: function() {
+				this.$http.get('data/studio.json').then(function(response) {
+					this.h_grade_three = response.body.h_grade_three; 
+				}, function(error) {
+					console.log(error);
+				});
+			},
+			methods: {
+				transmitCurrent: function(index) {   // 接收内容栏派发过来的事件，调用课程菜单栏的事件并将参数传递过去
+					this.$refs.nav.outIndex(index);
+				},
+				activeIndex: function(activeIndex) {  // 接收课程导航栏派发过来的事件，调用内容栏的事件并将参数传递过去
+					this.$refs.content.outIndex(activeIndex);
+				}
+			},
+			components: {
+				'v-subject-nav': SUBJECTNAV,
+				'v-subject-content': SUBJECTCONTENT
+			}
 		}
 		/* END */
 		
@@ -399,10 +823,16 @@
 		var COVER = {
 			template: '#cover-wrapper',
 			created: function() {
-				
 				this.$nextTick(function() {
 					this._calcNavWidth();
 				});
+			},
+			data: function() {
+				return {
+					msg: '学习天地',
+					search: true,
+					plus: true,
+				}
 			},
 			methods: {
 				_initNavScroll: function() {
@@ -430,7 +860,13 @@
 		
 		/* 个人 */
 		var MINE = {
-			template: '#mine-wrapper'
+			template: '#mine-wrapper',
+			data: function() {
+				return {
+					msg: '个人设置',
+					camera: true
+				}
+			}
 		};
 		/*END*/
 		
